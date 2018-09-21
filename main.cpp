63ff73fb3f417b8eb8c52fd898cc2d08d9c2483e
I love ass <roman.gurov0@gmail.com>
2018-09-21 13:42:47 +0300

    Added hw0

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using std::cout;
+
+
+int main() {
+	cout << "Bo! What the hell are you doing here? I told you last time you came by the station drunk as all hell - not to come by!"
+}
