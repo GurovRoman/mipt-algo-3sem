2a5883d19bc96583d2b51c45701cfaf2ffba4ed9
Роман Гуров <null>
2018-11-14 04:42:47 +0300

    Fixed all the issues commented.

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -16,115 +16,126 @@ private:
         int rank_next;
     };
 
+    void constructSuffix();
+    void constructLCP();
+
 public:
-    size_t operator[](size_t pos) {
-        return suffixArray[pos];
-    }
+    SuffixArrayWithLCP() = default;
+    explicit SuffixArrayWithLCP(std::string input);
 
-    size_t lcp(size_t pos) {
-        return lcpArray[pos];
-    }
+    size_t operator[](size_t pos) const;
+    size_t lcp(size_t pos) const;
+};
 
-    void constructFromString(const std::string &input) {
-        string = input;
-        constructSuffix();
-        constructLCP();
-    }
 
-    void constructSuffix() {
-        size_t size = string.size();
-        vector<RankedSuffix> suffixes(size);
-        for (size_t i = 0; i < size; ++i) {
-            suffixes[i].index = i;
-        }
-        for (size_t cycle_size = 2; cycle_size < 2 * size; cycle_size <<= 1) {
-            if (cycle_size == 2) {
-                for (size_t i = 0; i < size; ++i) {
-                    suffixes[i].rank = string[i];
-                    suffixes[i].rank_next = i + 1 < size ? string[i + 1] : -1;
-                }
+void SuffixArrayWithLCP::constructSuffix() {
+    const size_t size = string.size();
+    vector<RankedSuffix> suffixes(size);
+    for (size_t i = 0; i < size; ++i) {
+        suffixes[i].index = i;
+    }
+    for (size_t cycle_size = 2; cycle_size < 2 * size; cycle_size <<= 1) {
+        if (cycle_size == 2) {
+            for (size_t i = 0; i < size; ++i) {
+                suffixes[i].rank = string[i];
+                suffixes[i].rank_next = i + 1 < size ? string[i + 1] : -1;
             }
-            else {
-                int prev_rank = 0;
-                vector<size_t> index_to_suffix(size);
-                for (size_t i = 0; i < size; ++i) {
-                    if (i == 0) {
-                        prev_rank = suffixes[i].rank;
-                        suffixes[i].rank = 0;
+        }
+        else {
+            int prev_rank = 0;
+            vector<size_t> index_to_suffix(size);
+            for (size_t i = 0; i < size; ++i) {
+                if (i == 0) {
+                    prev_rank = suffixes[i].rank;
+                    suffixes[i].rank = 0;
+                }
+                else {
+                    if (suffixes[i].rank == prev_rank &&
+                        suffixes[i].rank_next == suffixes[i - 1].rank_next)
+                    {
+                        suffixes[i].rank = suffixes[i - 1].rank;
                     }
                     else {
-                        if (suffixes[i].rank == prev_rank &&
-                            suffixes[i].rank_next == suffixes[i - 1].rank_next) {
-                            suffixes[i].rank = suffixes[i - 1].rank;
-                        }
-                        else {
-                            prev_rank = suffixes[i].rank;
-                            suffixes[i].rank = suffixes[i - 1].rank + 1;
-                        }
-                        index_to_suffix[suffixes[i].index] = i;
+                        prev_rank = suffixes[i].rank;
+                        suffixes[i].rank = suffixes[i - 1].rank + 1;
                     }
-                }
-                for (size_t i = 0; i < size; ++i) {
-                    size_t next_index = suffixes[i].index + cycle_size / 2;
-                    suffixes[i].rank_next = next_index < size
-                                            ? suffixes[index_to_suffix[next_index]].rank
-                                            : -1;
+                    index_to_suffix[suffixes[i].index] = i;
                 }
             }
-
-            for (int rank_type = 0; rank_type < 2; ++rank_type) {
-                vector<RankedSuffix> buffer(size);
-                vector<size_t> counter(size > 254 ? size + 1 : 255, 0);
-                for (int i = 0; i < size; ++i) {
-                    int value = rank_type ? suffixes[i].rank : suffixes[i].rank_next;
-                    ++counter[value + 1];
-                }
-                for (int i = 1; i < counter.size(); ++i) {
-                    counter[i] += counter[i - 1];
-                }
-                for (size_t i = size; i > 0; --i) {
-                    int value = rank_type ? suffixes[i - 1].rank : suffixes[i - 1].rank_next;
-                    buffer[counter[value + 1] - 1] = suffixes[i - 1];
-                    counter[value + 1]--;
-                }
-                for (size_t i = 0; i < size; i++)
-                    suffixes[i] = buffer[i];
+            for (size_t i = 0; i < size; ++i) {
+                size_t next_index = suffixes[i].index + cycle_size / 2;
+                suffixes[i].rank_next = next_index < size
+                                        ? suffixes[index_to_suffix[next_index]].rank
+                                        : -1;
             }
+        }
 
-            suffixArray.resize(size);
+        for (int rank_type = 0; rank_type < 2; ++rank_type) {
+            vector<RankedSuffix> buffer(size);
+            vector<size_t> counter(std::max<size_t>(size + 1, 255));  //first rank is based on character ascii code, so may exceed size of the string
             for (int i = 0; i < size; ++i) {
-                suffixArray[i] = suffixes[i].index;
+                int value = rank_type ? suffixes[i].rank : suffixes[i].rank_next;
+                ++counter[value + 1];
+            }
+            for (int i = 1; i < counter.size(); ++i) {
+                counter[i] += counter[i - 1];
+            }
+            for (size_t i = size; i > 0; --i) {
+                int value = rank_type ? suffixes[i - 1].rank : suffixes[i - 1].rank_next;
+                buffer[counter[value + 1] - 1] = suffixes[i - 1];
+                counter[value + 1]--;
+            }
+            for (size_t i = 0; i < size; i++) {
+                suffixes[i] = buffer[i];
             }
         }
-    }
 
-    void constructLCP() {
-        size_t size = suffixArray.size();
-        lcpArray.resize(size);
-        vector<size_t> index_to_suffix(size);
-        for (size_t i = 0; i < size; ++i) {
-            index_to_suffix[suffixArray[i]] = i;
-        }
-        size_t prev_lcp = 0;
-        for (int current = 0; current < size; ++current) {
-            if (index_to_suffix[current] == size - 1) {
-                prev_lcp = 0;
-            }
-            else {
-                if (prev_lcp > 0) {
-                    --prev_lcp;
-                }
-                size_t next = suffixArray[index_to_suffix[current] + 1];
-                while (current + prev_lcp < size
-                       && next + prev_lcp < size
-                       && string[current + prev_lcp] == string[next + prev_lcp]) {
-                    ++prev_lcp;
-                }
-            }
-            lcpArray[index_to_suffix[current]] = prev_lcp;
+        suffixArray.resize(size);
+        for (int i = 0; i < size; ++i) {
+            suffixArray[i] = suffixes[i].index;
         }
     }
-};
+}
+
+void SuffixArrayWithLCP::constructLCP() {
+    const size_t size = suffixArray.size();
+    lcpArray.resize(size);
+    vector<size_t> index_to_suffix(size);
+    for (size_t i = 0; i < size; ++i) {
+        index_to_suffix[suffixArray[i]] = i;
+    }
+    size_t prev_lcp = 0;
+    for (int current = 0; current < size; ++current) {
+        if (index_to_suffix[current] == size - 1) {
+            prev_lcp = 0;
+        }
+        else {
+            if (prev_lcp > 0) {
+                --prev_lcp;
+            }
+            size_t next = suffixArray[index_to_suffix[current] + 1];
+            while (current + prev_lcp < size
+                   && next + prev_lcp < size
+                   && string[current + prev_lcp] == string[next + prev_lcp]) {
+                ++prev_lcp;
+            }
+        }
+        lcpArray[index_to_suffix[current]] = prev_lcp;
+    }
+}
+
+SuffixArrayWithLCP::SuffixArrayWithLCP(std::string input) : string(std::move(input)) {
+    constructSuffix();
+    constructLCP();
+}
+
+size_t SuffixArrayWithLCP::operator[](size_t pos) const {
+    return suffixArray[pos];
+}
+
+size_t SuffixArrayWithLCP::lcp(size_t pos) const {
+    return lcpArray[pos];
+}
 
 
 class TwoSuffixTrie {
@@ -157,9 +168,9 @@ private:
     }
 
 public:
-    void buildFromTwoStrings(const std::string& first, const std::string& second) {
+    TwoSuffixTrie(const std::string& first, const std::string& second) {
         std::string string = first + second;
-        suf.constructFromString(string);
+        suf = SuffixArrayWithLCP(string);
         trie.emplace_back(0, 0, 0, 0);
         size_t depth = 0;
         size_t current_node = 0;
@@ -212,7 +223,7 @@ public:
         printLexOrderRec(node_id);
     }
 
-    size_t getNodeCount() {
+    size_t getNodeCount() const {
         return trie.size();
     }
 };
@@ -220,8 +231,7 @@ public:
 int main() {
     std::string first, second;
     std::cin >> first >> second;
-    TwoSuffixTrie trie;
-    trie.buildFromTwoStrings(first, second);
+    TwoSuffixTrie trie(first, second);
     std::cout << trie.getNodeCount() << '\n';
     trie.printLexOrder(0);
     return 0;
--- task_3/main.cpp
+++ task_3/main.cpp
@@ -16,120 +16,131 @@ private:
         int rank_next;
     };
 
+    void constructSuffix();
+    void constructLCP();
+
 public:
-    size_t operator[](size_t pos) {
-        return suffixArray[pos];
-    }
+    SuffixArrayWithLCP() = default;
+    explicit SuffixArrayWithLCP(std::string input);
 
-    size_t lcp(size_t pos) {
-        return lcpArray[pos];
-    }
-
-    void constructFromString(const std::string &input) {
-        string = input;
-        constructSuffix();
-        constructLCP();
-    }
-
-    void constructSuffix() {
-        size_t size = string.size();
-        vector<RankedSuffix> suffixes(size);
-        for (size_t i = 0; i < size; ++i) {
-            suffixes[i].index = i;
-        }
-        for (size_t cycle_size = 2; cycle_size < 2 * size; cycle_size <<= 1) {
-            if (cycle_size == 2) {
-                for (size_t i = 0; i < size; ++i) {
-                    suffixes[i].rank = string[i];
-                    suffixes[i].rank_next = i + 1 < size ? string[i + 1] : -1;
-                }
-            }
-            else {
-                int prev_rank = 0;
-                vector<size_t> index_to_suffix(size);
-                for (size_t i = 0; i < size; ++i) {
-                    if (i == 0) {
-                        prev_rank = suffixes[i].rank;
-                        suffixes[i].rank = 0;
-                    }
-                    else {
-                        if (suffixes[i].rank == prev_rank &&
-                            suffixes[i].rank_next == suffixes[i - 1].rank_next) {
-                            suffixes[i].rank = suffixes[i - 1].rank;
-                        }
-                        else {
-                            prev_rank = suffixes[i].rank;
-                            suffixes[i].rank = suffixes[i - 1].rank + 1;
-                        }
-                        index_to_suffix[suffixes[i].index] = i;
-                    }
-                }
-                for (size_t i = 0; i < size; ++i) {
-                    size_t next_index = suffixes[i].index + cycle_size / 2;
-                    suffixes[i].rank_next = next_index < size
-                                            ? suffixes[index_to_suffix[next_index]].rank
-                                            : -1;
-                }
-            }
-
-            for (int rank_type = 0; rank_type < 2; ++rank_type) {
-                vector<RankedSuffix> buffer(size);
-                vector<size_t> counter(size > 254 ? size + 1 : 255, 0);
-                for (int i = 0; i < size; ++i) {
-                    int value = rank_type ? suffixes[i].rank : suffixes[i].rank_next;
-                    ++counter[value + 1];
-                }
-                for (int i = 1; i < counter.size(); ++i) {
-                    counter[i] += counter[i - 1];
-                }
-                for (size_t i = size; i > 0; --i) {
-                    int value = rank_type ? suffixes[i - 1].rank : suffixes[i - 1].rank_next;
-                    buffer[counter[value + 1] - 1] = suffixes[i - 1];
-                    counter[value + 1]--;
-                }
-                for (size_t i = 0; i < size; i++)
-                    suffixes[i] = buffer[i];
-            }
-
-            suffixArray.resize(size);
-            for (int i = 0; i < size; ++i) {
-                suffixArray[i] = suffixes[i].index;
-            }
-        }
-    }
-
-    void constructLCP() {
-        size_t size = suffixArray.size();
-        lcpArray.reserve(size);
-        vector<size_t> index_to_suffix(size);
-        for (size_t i = 0; i < size; ++i) {
-            index_to_suffix[suffixArray[i]] = i;
-        }
-        size_t prev_lcp = 0;
-        for (int current = 0; current < size; ++current) {
-            if (index_to_suffix[current] == size - 1) {
-                prev_lcp = 0;
-            }
-            else {
-                if (prev_lcp > 0) {
-                    --prev_lcp;
-                }
-                size_t next = suffixArray[index_to_suffix[current] + 1];
-                while (current + prev_lcp < size
-                       && next + prev_lcp < size
-                       && string[current + prev_lcp] == string[next + prev_lcp]) {
-                    ++prev_lcp;
-                }
-            }
-            lcpArray[index_to_suffix[current]] = prev_lcp;
-        }
-    }
+    size_t operator[](size_t pos) const;
+    size_t lcp(size_t pos) const;
 };
 
-std::string getKthCommonSubstring(const std:: string& first, const std:: string& second, unsigned long long k) {
+
+void SuffixArrayWithLCP::constructSuffix() {
+    const size_t size = string.size();
+    vector<RankedSuffix> suffixes(size);
+    for (size_t i = 0; i < size; ++i) {
+        suffixes[i].index = i;
+    }
+    for (size_t cycle_size = 2; cycle_size < 2 * size; cycle_size <<= 1) {
+        if (cycle_size == 2) {
+            for (size_t i = 0; i < size; ++i) {
+                suffixes[i].rank = string[i];
+                suffixes[i].rank_next = i + 1 < size ? string[i + 1] : -1;
+            }
+        }
+        else {
+            int prev_rank = 0;
+            vector<size_t> index_to_suffix(size);
+            for (size_t i = 0; i < size; ++i) {
+                if (i == 0) {
+                    prev_rank = suffixes[i].rank;
+                    suffixes[i].rank = 0;
+                }
+                else {
+                    if (suffixes[i].rank == prev_rank &&
+                        suffixes[i].rank_next == suffixes[i - 1].rank_next)
+                    {
+                        suffixes[i].rank = suffixes[i - 1].rank;
+                    }
+                    else {
+                        prev_rank = suffixes[i].rank;
+                        suffixes[i].rank = suffixes[i - 1].rank + 1;
+                    }
+                    index_to_suffix[suffixes[i].index] = i;
+                }
+            }
+            for (size_t i = 0; i < size; ++i) {
+                size_t next_index = suffixes[i].index + cycle_size / 2;
+                suffixes[i].rank_next = next_index < size
+                                        ? suffixes[index_to_suffix[next_index]].rank
+                                        : -1;
+            }
+        }
+
+        for (int rank_type = 0; rank_type < 2; ++rank_type) {
+            vector<RankedSuffix> buffer(size);
+            vector<size_t> counter(std::max<size_t>(size + 1, 255));  //first rank is based on character ascii code, so may exceed size of the string
+            for (int i = 0; i < size; ++i) {
+                int value = rank_type ? suffixes[i].rank : suffixes[i].rank_next;
+                ++counter[value + 1];
+            }
+            for (int i = 1; i < counter.size(); ++i) {
+                counter[i] += counter[i - 1];
+            }
+            for (size_t i = size; i > 0; --i) {
+                int value = rank_type ? suffixes[i - 1].rank : suffixes[i - 1].rank_next;
+                buffer[counter[value + 1] - 1] = suffixes[i - 1];
+                counter[value + 1]--;
+            }
+            for (size_t i = 0; i < size; i++) {
+                suffixes[i] = buffer[i];
+            }
+        }
+
+        suffixArray.resize(size);
+        for (int i = 0; i < size; ++i) {
+            suffixArray[i] = suffixes[i].index;
+        }
+    }
+}
+
+void SuffixArrayWithLCP::constructLCP() {
+    const size_t size = suffixArray.size();
+    lcpArray.resize(size);
+    vector<size_t> index_to_suffix(size);
+    for (size_t i = 0; i < size; ++i) {
+        index_to_suffix[suffixArray[i]] = i;
+    }
+    size_t prev_lcp = 0;
+    for (int current = 0; current < size; ++current) {
+        if (index_to_suffix[current] == size - 1) {
+            prev_lcp = 0;
+        }
+        else {
+            if (prev_lcp > 0) {
+                --prev_lcp;
+            }
+            size_t next = suffixArray[index_to_suffix[current] + 1];
+            while (current + prev_lcp < size
+                   && next + prev_lcp < size
+                   && string[current + prev_lcp] == string[next + prev_lcp]) {
+                ++prev_lcp;
+            }
+        }
+        lcpArray[index_to_suffix[current]] = prev_lcp;
+    }
+}
+
+SuffixArrayWithLCP::SuffixArrayWithLCP(std::string input) : string(std::move(input)) {
+    constructSuffix();
+    constructLCP();
+}
+
+size_t SuffixArrayWithLCP::operator[](size_t pos) const {
+    return suffixArray[pos];
+}
+
+size_t SuffixArrayWithLCP::lcp(size_t pos) const {
+    return lcpArray[pos];
+}
+
+
+std::string getKthCommonSubstring(const std::string& first, const std:: string& second, unsigned long long k) {
     std::string string = first + "#" + second;
-    SuffixArrayWithLCP suf;
-    suf.constructFromString(string);
+    SuffixArrayWithLCP suf(string);
     size_t current = 0;
     size_t prev_lcp = 0;
     size_t i;
@@ -147,9 +158,7 @@ std::string getKthCommonSubstring(const std:: string& first, const std:: string&
     if (current < k) {
         return "-1";
     }
-    else {
-         return string.substr(suf[i - 1], k - (current - prev_lcp));
-    }
+    return string.substr(suf[i - 1], k - (current - prev_lcp));
 }
 
 int main() {
